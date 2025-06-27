62d37fb03c3f83513440e8f0f5e9892988d9b470
vbreddy9 <111076954+vbreddy9@users.noreply.github.com>
2025-06-27 15:11:31 +0530

    Update AutoPopup.jsx

--- frontend/src/Components/AutoPopup.jsx
+++ frontend/src/Components/AutoPopup.jsx
@@ -52,11 +52,12 @@ const AutoPopup = ({ isOpen, onClose, title }) => {
     };
 
     try {
-      const response = await fetch('https://vr-2-tech-frontend.vercel.app/api/home/send-email', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(payload)
-      });
+     const response = await fetch('https://vr-2-tech-backend.vercel.app/api/home/send-email', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(payload)
+        });
+
 
       const data = await response.json();
 
